34bd0e982823168e3e585f14c5b54b4c710a4aca
abrilknudsen <knudsenabril@gmail.com>
2025-10-04 14:44:12 -0300

    sketch.js

--- sketch.js
+++ sketch.js
@@ -5,8 +5,8 @@ let FREC_MIN = 120;  //AGUDO-GRAVE
 let FREC_MAX = 900;
 let FREC;
 
-let AMP_MIN = 0.03; //FUERTE,DEBIL,SUAVE    /////////CHEQUEAR MAÑANA
-let AMP_MAX = 7;
+let AMP_MIN = 0.001; //FUERTE,DEBIL,SUAVE    /////////CHEQUEAR MAÑANA
+let AMP_MAX = 0.9;
 let AMP;
 
 let mic;
